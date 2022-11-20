09eddd836116eabceefaa72a6724f1e70048f4c4
Pranav Vashi <neobuddy89@gmail.com>
2022-11-21 02:14:55 +0530

    WitAquaMagic: Switch to SystemPropertySwitchPreference
    Change-Id: I82c30d263096e1d45d991dcdce7200321a86c2d9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/org/witaqua/settings/fragments/spoofing/Spoofing.java
+++ src/org/witaqua/settings/fragments/spoofing/Spoofing.java
@@ -22,14 +22,12 @@ import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
-import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.internal.util.witaqua.KeyProviderManager;
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settingslib.development.SystemPropPoker;
 import com.android.settingslib.search.SearchIndexable;
 
 import org.witaqua.settings.preferences.KeyboxDataPreference;
@@ -43,12 +41,9 @@ public class Spoofing extends SettingsPreferenceFragment implements
 
     private static final String TAG = "Spoofing";
 
-    private static final String KEY_GAMES_SPOOF = "use_games_spoof";
     private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
-    private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
     private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
-    private static final String KEY_NETFLIX_SPOOF = "use_netflix_spoof";
-    private static final String SYS_NETFLIX_SPOOF = "persist.sys.spoof_netflix";
+    private static final String SYS_NETFLIX_SPOOF = "persist.sys.pixelprops.netflix";
     private static final String SYS_PI_SPOOF = "persist.sys.pixelprops.pi";
     private static final String SYS_GMS_CERT_SPOOF = "persist.sys.pixelprops.gmscertchain";
     private static final String KEYBOX_DATA_KEY = "keybox_data_setting";
@@ -56,9 +51,6 @@ public class Spoofing extends SettingsPreferenceFragment implements
     private ActivityResultLauncher<Intent> mKeyboxFilePickerLauncher;
     private SystemPropertySwitchPreference mDisableForceIntegrity;
     private KeyboxDataPreference mKeyboxDataPreference;
-    private SwitchPreference mGamesSpoof;
-    private SwitchPreference mPhotosSpoof;
-    private SwitchPreference mNetFlixSpoof;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -69,18 +61,6 @@ public class Spoofing extends SettingsPreferenceFragment implements
         final ContentResolver resolver = context.getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources resources = context.getResources();
-
-        mGamesSpoof = (SwitchPreference) prefScreen.findPreference(KEY_GAMES_SPOOF);
-        mGamesSpoof.setChecked(SystemProperties.getBoolean(SYS_GAMES_SPOOF, false));
-        mGamesSpoof.setOnPreferenceChangeListener(this);
-
-        mPhotosSpoof = (SwitchPreference) prefScreen.findPreference(KEY_PHOTOS_SPOOF);
-        mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
-        mPhotosSpoof.setOnPreferenceChangeListener(this);
-
-        mNetFlixSpoof = (SwitchPreference) findPreference(KEY_NETFLIX_SPOOF);
-        mNetFlixSpoof.setChecked(SystemProperties.getBoolean(SYS_NETFLIX_SPOOF, false));
-        mNetFlixSpoof.setOnPreferenceChangeListener(this);
 
         mDisableForceIntegrity = findPreference(SYS_GMS_CERT_SPOOF);
         if (mDisableForceIntegrity != null) {
@@ -108,22 +88,6 @@ public class Spoofing extends SettingsPreferenceFragment implements
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final Context context = getContext();
         final ContentResolver resolver = context.getContentResolver();
-        if (preference == mGamesSpoof) {
-            boolean value = (Boolean) newValue;
-            SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
-            SystemPropPoker.getInstance().poke();
-            return true;
-        } else if (preference == mPhotosSpoof) {
-            boolean value = (Boolean) newValue;
-            SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
-            SystemPropPoker.getInstance().poke();
-            return true;
-        } else if (preference == mNetFlixSpoof) {
-            boolean value = (Boolean) newValue;
-            SystemProperties.set(SYS_NETFLIX_SPOOF, value ? "true" : "false");
-            SystemPropPoker.getInstance().poke();
-            return true;
-        }
         return false;
     }
 
