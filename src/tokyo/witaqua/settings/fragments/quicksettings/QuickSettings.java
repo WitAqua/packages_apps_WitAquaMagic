b7a29c89bdd7128c2aace198e67a750892dfc805
ShevT <evgeny.shishkov@gmail.com>
2024-05-19 20:40:23 +0900

    WitAquaMagic: Add quick settings brightness slider customizations
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I72accc553a21d60a1fa27ef1d6e400af3987f25b
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- src/tokyo/witaqua/settings/fragments/quicksettings/QuickSettings.java
+++ src/tokyo/witaqua/settings/fragments/quicksettings/QuickSettings.java
@@ -12,10 +12,12 @@ import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.Settings;
 
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
@@ -25,7 +27,11 @@ import com.android.settingslib.search.SearchIndexable;
 
 import java.util.List;
 
+import lineageos.preference.LineageSecureSettingSwitchPreference;
+import lineageos.providers.LineageSettings;
+
 import tokyo.witaqua.settings.preferences.SystemSettingListPreference;
+import tokyo.witaqua.settings.preferences.SystemSettingSwitchPreference;
 
 @SearchIndexable
 public class QuickSettings extends SettingsPreferenceFragment implements
@@ -33,8 +39,12 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
     private static final String TAG = "QuickSettings";
 
+    private static final String KEY_BRIGHTNESS_SLIDER_POSITION = "qs_brightness_slider_position";
+    private static final String KEY_BRIGHTNESS_SLIDER_HAPTIC = "qs_brightness_slider_haptic";
     private static final String KEY_BATTERY_PERCENT = "qs_show_battery_percent";
     private static final String KEY_BATTERY_STYLE = "qs_battery_style";
+    private static final String KEY_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
+    private static final String KEY_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
 
     private static final int BATTERY_STYLE_PORTRAIT = 0;
     private static final int BATTERY_STYLE_TEXT = 4;
@@ -42,6 +52,10 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
     private SystemSettingListPreference mBatteryStyle;
     private SystemSettingListPreference mBatteryPercent;
+    private ListPreference mShowBrightnessSlider;
+    private ListPreference mBrightnessSliderPosition;
+    private LineageSecureSettingSwitchPreference mShowAutoBrightness;
+    private SystemSettingSwitchPreference mBrightnessSliderHaptic;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -63,6 +77,26 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
         mBatteryPercent.setEnabled(
                 batterystyle != BATTERY_STYLE_TEXT && batterystyle != BATTERY_STYLE_HIDDEN);
+        
+        mShowBrightnessSlider = findPreference(KEY_SHOW_BRIGHTNESS_SLIDER);
+        mShowBrightnessSlider.setOnPreferenceChangeListener(this);
+        boolean showSlider = LineageSettings.Secure.getIntForUser(resolver,
+                LineageSettings.Secure.QS_SHOW_BRIGHTNESS_SLIDER, 1, UserHandle.USER_CURRENT) > 0;
+
+        mBrightnessSliderPosition = findPreference(KEY_BRIGHTNESS_SLIDER_POSITION);
+        mBrightnessSliderPosition.setEnabled(showSlider);
+
+        mBrightnessSliderHaptic = findPreference(KEY_BRIGHTNESS_SLIDER_HAPTIC);
+        mBrightnessSliderHaptic.setEnabled(showSlider);
+
+        mShowAutoBrightness = findPreference(KEY_SHOW_AUTO_BRIGHTNESS);
+        boolean automaticAvailable = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_automatic_brightness_available);
+        if (automaticAvailable) {
+            mShowAutoBrightness.setEnabled(showSlider);
+        } else {
+            prefScreen.removePreference(mShowAutoBrightness);
+        }
     }
 
     @Override
@@ -73,6 +107,13 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             int value = Integer.parseInt((String) newValue);
             mBatteryPercent.setEnabled(
                     value != BATTERY_STYLE_TEXT && value != BATTERY_STYLE_HIDDEN);
+            return true;
+        } else if (preference == mShowBrightnessSlider) {
+            int value = Integer.parseInt((String) newValue);
+            mBrightnessSliderPosition.setEnabled(value > 0);
+            mBrightnessSliderHaptic.setEnabled(value > 0);
+            if (mShowAutoBrightness != null)
+                mShowAutoBrightness.setEnabled(value > 0);
             return true;
         }
         return false;
@@ -90,6 +131,12 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             public List<String> getNonIndexableKeys(Context context) {
                 List<String> keys = super.getNonIndexableKeys(context);
                 final Resources resources = context.getResources();
+
+                boolean autoBrightnessAvailable = resources.getBoolean(
+                        com.android.internal.R.bool.config_automatic_brightness_available);
+                if (!autoBrightnessAvailable) {
+                    keys.add(KEY_SHOW_AUTO_BRIGHTNESS);
+                }
                 return keys;
             }
         };
