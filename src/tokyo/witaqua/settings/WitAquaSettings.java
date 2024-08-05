a3c8de543bdb11703fded1b70dabc7feaf842e3c
Toufu <hamm157famm7423@gmail.com>
2024-08-05 23:32:00 +0900

    WitAquaMagic: Initial commit * Used the crDroid, Evolution X, ProjectBlaze and 2by2 implementation as a reference.
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Anierin Bliss <anierin@evolution-x.org>
    Co-authored-by: Joey <joey@evolution-x.org>
    Co-authored-by: Aditya Pratap Singh <shivamatiet2001@gmail.com>
    Co-authored-by: lahaina <nattolecats@gmail.com>
    
     Author:    toufu <hamm157famm7423@gmail.com>

---
+++ src/tokyo/witaqua/settings/WitAquaSettings.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2024 WitAqua
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tokyo.witaqua.settings;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+@SearchIndexable
+public class WitAquaSettings extends DashboardFragment {
+
+    private static final String TAG = "WitAquaMagic";
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.witaqua_settings;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.WITAQUA_SETTINGS;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.witaqua_settings);
+}
