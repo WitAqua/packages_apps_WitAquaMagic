Evolution-X/packages_apps_Evolver

Evolver: Import TelephonyUtils from LineageOS
Co-authored-by: Arne Coucheron <arco68@gmail.com>
Co-authored-by: LuK1337 <priv.luk@gmail.com>
Signed-off-by: AnierinB <anierin@evolution-x.org>

src/org/evolution/settings/utils/TelephonyUtils.java
@@ -1,0 +1,24 @@
+/*
+ * SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.evolution.settings.utils;
+
+import android.content.Context;
+import android.telephony.TelephonyManager;
+
+import androidx.annotation.NonNull;
+
+public class TelephonyUtils {
+
+    private static final String TAG = TelephonyUtils.class.getSimpleName();
+
+    /**
+     * Returns whether the device is voice-capable (meaning, it is also a phone).
+     */
+    public static boolean isVoiceCapable(@NonNull Context context) {
+        TelephonyManager telephony = context.getSystemService(TelephonyManager.class);
+        return telephony != null && telephony.isVoiceCapable();
+    }
+}

WitAqua/packages_apps_WitAquaMagic

Change-Id: I2f57e59396e4ae432c6ff68b1579be1e576331e7

src/tokyo/witaqua/settings/utils/TelephonyUtils.java
@@ -1,0 +1,24 @@
+/*
+ * SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package tokyo.witaqua.settings.utils;
+
+import android.content.Context;
+import android.telephony.TelephonyManager;
+
+import androidx.annotation.NonNull;
+
+public class TelephonyUtils {
+
+    private static final String TAG = TelephonyUtils.class.getSimpleName();
+
+    /**
+     * Returns whether the device is voice-capable (meaning, it is also a phone).
+     */
+    public static boolean isVoiceCapable(@NonNull Context context) {
+        TelephonyManager telephony = context.getSystemService(TelephonyManager.class);
+        return telephony != null && telephony.isVoiceCapable();
+    }
+}